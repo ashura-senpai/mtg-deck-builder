bbd337ba2f1c8c5e922bd473f5dce877b7b5e7fb
Ashura <rorikemp@gmail.com>
2024-08-13 21:57:51 -0300

    updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CommanderController } from './commander/commander.controller';
 import { CommanderService } from './commander/commander.service';
 import { CardsController } from './cards/cards.controller';
 import { CardsService } from './cards/cards.service';
+import { DeckController } from './deck/schemas/deck.controller';
+import { DeckService } from './deck/schemas/deck.service';
+import { Deck, DeckSchema } from './deck/schemas/deck.schema';
 
 @Module({
-  imports: [],
-  controllers: [CommanderController, CardsController],
-  providers: [CommanderService, CardsService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/mtg-deck-builder'),
+    MongooseModule.forFeature([{ name: Deck.name, schema: DeckSchema }]),
+  ],
+  controllers: [CommanderController, CardsController, DeckController],
+  providers: [CommanderService, CardsService, DeckService],
 })
 export class AppModule { }
--- src/cards/cards.service.ts
+++ src/cards/cards.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { DeckService } from '../deck/schemas/deck.service';
 import axios from 'axios';
 import * as fs from 'fs';
 
@@ -19,5 +20,20 @@ export class CardsService {
         fs.writeFileSync('deck.json', JSON.stringify(deck, null, 2)); // salvar em json
 
         return deck;
+    }
+
+    constructor(private readonly deckService: DeckService) { }
+
+    async getCardsByColorAndSaveDeck(commander: string, color: string): Promise<any> {
+        const response = await axios.get(this.mtgApiUrl, {
+            params: {
+                colors: color,
+                pageSize: 99,
+            },
+        });
+
+        const cards = response.data.cards;
+
+        return this.deckService.createDeck(commander, cards); // salvar no bd
     }
 }
