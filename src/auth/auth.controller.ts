dd6b09eb881e328eb6d8daf8c6a11c1d13bbd4a0
Ashura <rorikemp@gmail.com>
2024-08-19 22:10:12 -0300

    auth update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,17 +2,18 @@ import { Controller, Request, Post, UseGuards, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 import { UsersService } from '../users/users.service';
+import { CreateUserDto } from '../users/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
     constructor(
-        private authService: AuthService,
-        private usersService: UsersService
+        private readonly authService: AuthService,
+        private readonly usersService: UsersService,
     ) { }
 
     @Post('register')
-    async register(@Body() body) {
-        return this.usersService.create(body.username, body.password);
+    async register(@Body() createUserDto: CreateUserDto) {
+        return this.usersService.create(createUserDto);
     }
 
     @UseGuards(LocalAuthGuard)
